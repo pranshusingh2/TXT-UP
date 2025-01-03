642a6117a3e88eeb30b867e4aba215cce6e6ca16
Csk <159976535+csk895460@users.noreply.github.com>
2025-01-03 23:55:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from pyrogram import Client, filters
 from p_bar import progress_bar
 from subprocess import getstatusoutput
 from aiohttp import ClientSession
+import helper
 from helper import get_drm_keys
 from logger import logging
 import time
