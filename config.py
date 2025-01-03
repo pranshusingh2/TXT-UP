cb8aaac7cf7e5821d1db88b3e7afc559fc76e8b3
Csk <159976535+csk895460@users.noreply.github.com>
2025-01-03 23:03:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import os
 
 API_ID = API_ID =  
 
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "098112aae38be62db58363267a061b59")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8123485632:AAHTvq38wj0-8426bsE0TDqsYvGIYU_9JMA")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
-OWNER = int(os.environ.get("OWNER", ))
+OWNER = int(os.environ.get("5201266500", ))
 
 LOG = ,
 
